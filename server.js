2cd9f75d8a1feb66998e9ac456b78f1b1ea461fc
sathyanarayanan <sathyanarayanan1106@gmail.com>
2025-09-08 21:03:57 +0530

    update secret values

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 9090;
 
 app.get("/auto-deploy", (req, res) => {
   res.json({
